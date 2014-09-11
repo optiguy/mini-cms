87406f18c6e7c8eb28621bc4a1ac83d396721bcf
Bjarke B Rubeksen <mydunlet@gmail.com>
2014-09-11 09:34:28 +0200

    Added validation to user manager
    Added GUMP validation to user manager.

--- inc/admin/_admin_user.php
+++ inc/admin/_admin_user.php
@@ -1,6 +1,9 @@
 <?php if(!defined('BASE_URL')) die('No Script access is allowed'); ?>
 <?php
 	redirect_if_user(); //Redirect if user is not administrator - See inc/functions.php
+	
+	//Klargør
+	$user = (isset($_POST['edit_user']) and isset($_GET['user_id'])) ? ORM::for_table('users')->find_one($_GET['user_id']) : ORM::for_table('users')->create();
 
 	if(isset($_GET['task']) and $_GET['task'] == 'del')
 	{
@@ -23,69 +26,85 @@
 		}
 	} elseif( isset($_POST['save_user']) or isset($_POST['edit_user']) )
 	{
-		//Klargør 
-		$user = (isset($_POST['save_user'])) ? ORM::for_table('users')->create() : ORM::for_table('users')->find_one($_GET['user_id']);
+		$rules = array(
+		    'name' => 'required|valid_name',
+		    'email' => 'required|valid_email',
+		    'password' => 'max_len,100|min_len,6',
+		    'address' => 'alpha_numeric'
+		);
+		if( isset($_POST['save_user']) )
+			$rules['password'] = 'required|max_len,100|min_len,6';
 
-		$mime_type = array('jpg','jpeg','gif','png','bmp'); //Tilladte filtyper
-		$max_size = 1024*1024*5; //Max tilladte fil størrelse
-		$image = $_FILES['avatar']; //Gem billedet i en variabel
-		if( !empty($image['name']) ) //Hvis der er et billede der skal uploades
-		{
-			if( $image['error']==0 ) //Hvis billedet ikke indeholder fejl
+		$validator = new GUMP();
+		$_POST = $validator->sanitize($_POST); //Sanitize all values
+		$validator->validation_rules($rules);
+
+		if(!$validator->run($_POST)):
+		     foreach($validator->get_readable_errors() as $error)
+	     		set_message('user',$error);
+		else:
+			
+			$mime_type = array('jpg','jpeg','gif','png','bmp'); //Tilladte filtyper
+			$max_size = 1024*1024*5; //Max tilladte fil størrelse
+			$image = $_FILES['avatar']; //Gem billedet i en variabel
+			if( !empty($image['name']) ) //Hvis der er et billede der skal uploades
 			{
-				$mime = pathinfo($image['name'], PATHINFO_EXTENSION); //Hent fil endelse
-				if( !in_array($mime, $mime_type) ) //Tjek på om filendelse er gyldig
+				if( $image['error']==0 ) //Hvis billedet ikke indeholder fejl
 				{
-					set_message('avatar','Filen er ikke et billede'); //Sæt fejlbesked - See inc/functions.php
+					$mime = pathinfo($image['name'], PATHINFO_EXTENSION); //Hent fil endelse
+					if( !in_array($mime, $mime_type) ) //Tjek på om filendelse er gyldig
+					{
+						set_message('avatar','Filen er ikke et billede'); //Sæt fejlbesked - See inc/functions.php
+						redirect_to(BASE_URL.'?page=admin_user');//Redirect og vis besked
+					}
+					if( $image['size'] > $max_size )
+					{
+						set_message('avatar','Filen er for stor');//Sæt fejlbesked - See inc/functions.php
+						redirect_to(BASE_URL.'?page=admin_user');//Redirect og vis besked
+					}
+					if( !empty($user->avatar) ) //Hvis brugeren allerede har et billede
+						unlink('uploads/avatars/'.$user->avatar); //Slet billedet
+
+					$filename = time().'_'.$image['name']; //Lav nyt filnavn til billedet
+					WideImage::load($image['tmp_name'])->resize(150,150,'outside')->crop('center','center',150,150)->saveToFile('uploads/avatars/'.$filename);
+					$user->avatar = $filename; //Gem i database
+
+				} else {
+					set_message('avatar','Billedet indeholder fejl!');//Sæt fejlbesked - See inc/functions.php
 					redirect_to(BASE_URL.'?page=admin_user');//Redirect og vis besked
 				}
-				if( $image['size'] > $max_size )
-				{
-					set_message('avatar','Filen er for stor');//Sæt fejlbesked - See inc/functions.php
-					redirect_to(BASE_URL.'?page=admin_user');//Redirect og vis besked
-				}
-				if( !empty($user->avatar) ) //Hvis brugeren allerede har et billede
-					unlink('uploads/avatars/'.$user->avatar); //Slet billedet
-
-				$filename = time().'_'.$image['name']; //Lav nyt filnavn til billedet
-				WideImage::load($image['tmp_name'])->resize(150,150,'outside')->crop('center','center',150,150)->saveToFile('uploads/avatars/'.$filename);
-				$user->avatar = $filename; //Gem i database
-
-			} else {
-				set_message('avatar','Billedet indeholder fejl!');//Sæt fejlbesked - See inc/functions.php
-				redirect_to(BASE_URL.'?page=admin_user');//Redirect og vis besked
 			}
-		}
 
-		$user->name     = $_POST['name'];
-		$user->email    = $_POST['email'];
-		$user->password = better_crypt($_POST['password']);
-		$user->adresse  = $_POST['adresse'];
-		$user->rolle_id = $_POST['rolle'];
-		
-		if(!$user->save())
-		{
-			set_message('user','Kunne ikke gemme brugeren');
-			redirect_to(BASE_URL.'?page=admin_user');
-		} else {
-			set_message('user','Brugeren er oprettet');
-			redirect_to(BASE_URL.'?page=admin_user');
-		}
-		
+			if( isset($_POST['password']) ) $user->password = better_crypt($_POST['password']);
+			$user->name     = $_POST['name'];
+			$user->email    = $_POST['email'];
+			$user->adresse  = $_POST['adresse'];
+			$user->rolle_id = $_POST['rolle'];
+			
+			if(!$user->save())
+			{
+				set_message('user','Kunne ikke gemme brugeren');
+				redirect_to(BASE_URL.'?page=admin_user');
+			} else {
+				set_message('user','Brugeren er oprettet');
+				redirect_to(BASE_URL.'?page=admin_user');
+			}
+		endif;
 	}
 	$users = ORM::for_table('users')->select('users.*')->select('rolle.navn','rolename')->inner_join('rolle','rolle.id = users.rolle_id')->find_many();
 ?>
+<?php echo show_message('user'); ?>
 <?php if(isset($_GET['task']) and ($_GET['task'] == 'new' or $_GET['task'] == 'edit')): ?>
 <?php if($_GET['task'] == 'edit')
 		$user = ORM::for_table('users')->find_one($_GET['user_id']); ?>
 	<form method="post" class="form form-horizontal" enctype="multipart/form-data">
 		<div class="form-group">
 			<label for="name">Navn</label>
-			<input type="text" class="controle-group" value="<?php echo (isset($user->name))?$user->name:''?>" name="name" placeholder="Indsæt brugerens navn...">
+			<input type="text" class="controle-group" value="<?php echo (isset($_POST['name']))?$_POST['name']:$user->name?>" name="name" placeholder="Indsæt brugerens navn...">
 		</div>
 		<div class="form-group">
 			<label for="email">email</label>
-			<input type="email" class="controle-group" value="<?php echo (isset($user->email))?$user->email:''?>" name="email" placeholder="Indsæt brugerens navn...">
+			<input type="email" class="controle-group" value="<?php echo (isset($_POST['email']))?$_POST['email']:$user->email?>" name="email" placeholder="Indsæt brugerens navn...">
 		</div>
 		<div class="form-group">
 			<label for="password">password</label>
