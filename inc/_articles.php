bdca76718c59d84396eca4ffa8fb2e09c2e462ce
Bjarke B Rubeksen <mydunlet@gmail.com>
2014-09-22 11:40:44 +0200

    bug and subscription
    removed bug, where articles was doing an endless loop, if no content was found.
    
    Added feature for users to subscribe to content.

--- inc/_articles.php
+++ inc/_articles.php
@@ -4,11 +4,8 @@
 	$total_articles = count(ORM::for_table('content')->find_result_set()); //Total number og articles in database
 	$total_pages = ceil($total_articles/PER_PAGE); //Total number of page rounded up
 
-	if( $total_pages <= $page_num ) //Check if page exist
-	{
-		set_message('page','Der er flere sider!'); //Set error message
+	if( $total_pages <= $page_num and $total_pages > 0 ) //Check if page exist
 		redirect_to(BASE_URL.'?page=articles'); //Redirect to first page
-	}
 
 	//Get all articles limited to the page we're on
 	$articles = ORM::for_table('content')->limit(PER_PAGE)->offset($offset)->order_by_desc('date_created')->find_many();
@@ -23,6 +20,41 @@
 		set_message('rating','Rating er gemt på artiklen:'.$article_to_rate->title); //Set message
 		redirect_to(BASE_URL.'?page=articles'); //Redirect user to articles
 	}
+
+	//Subscribe
+	if( isset($_GET['subscribe']) and ($_GET['subscribe']=='on' or $_GET['subscribe'] == 'off') )
+	{
+		$is_subscribed = ORM::for_table('subscribe_to_content')->where(array(
+				'content_id'=> $_GET['content_id'],
+				'users_id' => $_SESSION['user']['id']
+				))->find_one();
+
+		if($_GET['subscribe']=='on')
+		{
+			if( $is_subscribed )
+			{
+				set_message('article','Du er allerede tilmeldt.');
+				redirect_to(BASE_URL.'?page=articles');
+			} else {
+				//Tilmeld bruger
+				$tilmelding = ORM::for_table('subscribe_to_content')->create();
+				$tilmelding->content_id = $_GET['content_id'];
+				$tilmelding->users_id = $_SESSION['user']['id'];
+				if($tilmelding->save())
+				{
+					set_message('article','Du er nu tilmeldt artiklen');
+					redirect_to(BASE_URL.'?page=articles');
+				}
+			}
+		} else {
+			//afmeld bruger
+			if($is_subscribed and $tilmelding->delete())
+			{
+				set_message('article','Du er nu afmeldt artiklen');
+				redirect_to(BASE_URL.'?page=articles');
+			}
+		}
+	}
 ?>
 <div class="row">
 	<div class="col-xs-12">
@@ -34,6 +66,7 @@
 				echo '<li class="'.$active.'"><a href="'.BASE_URL.'?page=articles&page_num='.$i.'">'.$i.'</a></li>';
 			}?>
 		</ul>
+
 		<?php if($total_articles == 0): ?>
 			<h2>Der er ingen artikler i øjeblikket.</h2>
 		<?php else: ?>
@@ -52,7 +85,21 @@
 						echo '<a href="'.BASE_URL.'?page=articles&article='.$article->id.'&rating='.$i.'"><span class="glyphicon glyphicon-star-empty"></span></a>';
 				}
 				?>
+
 				<?php echo $avg_rating ?>/<?php echo NUM_RATING ?>
+				<?php if( isset($_SESSION['user']) ): ?>
+					<?php
+						$is_subscribed = ORM::for_table('subscribe_to_content')->where(array(
+							'content_id'=> $article->id,
+							'users_id' => $_SESSION['user']['id']
+						))->find_one();
+					?>					
+					<?php if($is_subscribed){ ?>
+						<a href="<?php echo BASE_URL ?>?page=articles&content_id=<?php echo $article->id ?>&subscribe=off">Afmeld artikel</a>
+					<?php } else {?>
+						<a href="<?php echo BASE_URL ?>?page=articles&content_id=<?php echo $article->id ?>&subscribe=on">Abboner på artikel</a>
+					<?php } ?>
+				<?php endif; ?>
 				<hr>
 			<?php endforeach; ?>
 		<?php endif; ?>
